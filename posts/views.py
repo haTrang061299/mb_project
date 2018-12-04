a3152ab2cf206b2575d80783944f6f04a1b480c4
haTrang061299 <33522506+haTrang061299@users.noreply.github.com>
2018-12-04 15:17:43 -0500

    Add files via upload

---
+++ posts/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import ListView
+from .models import Post
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'index.html'
+    context_object_name = 'all_posts_list'
+
+
